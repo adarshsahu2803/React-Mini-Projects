fee918177af6918428d75bc247265bbf5137d527
Adarsh Sahu <adarshsahu1077@gmail.com>
2023-12-15 17:48:16 +0530

    Added component files

---
+++ src/todoItem.js
@@ -0,0 +1,14 @@
+
+
+function addItem() {
+    todoText = {
+        1:"Prepare sideEffect",
+        2:"Drink water",
+        3:"Go to gym"
+    }
+    return (
+        <button onClick={todoList}>Add Todo</button>
+    )
+}
+
+export default addItem;

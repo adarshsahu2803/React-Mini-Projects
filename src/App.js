183aede8e62c5ea0f502ac4f1bd6063f82cf9681
Adarsh Sahu <adarshsahu1077@gmail.com>
2023-12-20 01:37:38 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const message = () => {
+    return 'this is todolist.'
+  }
+
+  const msg1 = 'this is msg1'
+
   return (
     <div>
       <h1>Todo List</h1>
-      <todoList />
-      <addItem />
+      <p>{message()}</p>
+      {msg1}
     </div>
   );
 }

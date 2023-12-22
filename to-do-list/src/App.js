011c5dbb8b9a06f5346b689e4e09f1901bd3900d
Adarsh Sahu <adarshsahu1077@gmail.com>
2023-12-23 00:40:45 +0530

    single element delete

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (todoText) => {
-    setTodos([...todos, {text: todoText, completed: false}])
+    setTodos([...todos, {
+      id: uuidv4(),
+      text: todoText,
+      completed: false
+    }])
   }
 
   // Function to toggle the completed state of a todo
@@ -23,12 +28,19 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const updateTodo = (id, newText) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: newText } : todo
+    );
+    setTodos(updatedTodos);
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm addTodo={addTodo} />
       {console.log(todos)}
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
     </div>
   );
 }
--- to-do-list/src/todoList.js
+++ to-do-list/src/todoList.js
@@ -1,19 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 
-function TodoList({ todos, deleteTodo, toggleTodo }) {
+function TodoList({ todos, deleteTodo, toggleTodo, updateTodo }) {
+    const [editText, setEditText] = useState('');
+    const [editId, setEditId] = useState(null);
+
+    const handleEdit = (id, text) => {
+        setEditId(id);
+        setEditText(text);
+    }
+
+    const handleUpdate = (id) => {
+        if (editText.trim() !== '') {
+            updateTodo(id, editText);
+            setEditId(null);
+            setEditText('');
+        }
+    }
+
     return (
         <ul>
             {todos.map((todo) => (
                 <li key={todo.id}>
-                    <input
-                        type="checkbox"
-                        checked={todo.completed}
-                        onChange={() => toggleTodo(todo.id)}
-                    />
-                    <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-                        {todo.text}
-                    </span>
-                    <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    {editId === todo.id ? (
+                        <div>
+                            <input
+                                type="text"
+                                checked={todo.completed}
+                                onChange={() => toggleTodo(todo.id)}
+                            />
+                            <button onClick={() => handleUpdate(todo.id)}>Save</button>
+                        </div>) : (
+                        <div>
+                            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+                                {todo.text}
+                            </span>
+                            <button onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
+                            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        </div>
+                    )}
                 </li>
             ))}
         </ul>
